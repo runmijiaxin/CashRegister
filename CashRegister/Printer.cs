635f9051da2596f8525e25721fa42b109b04cf08
Jie Li <JLi147@slb.com>
2022-10-26 14:50:10 +0800

    test: add test Should_process_execute_printing V1

--- CashRegister/Printer.cs
+++ CashRegister/Printer.cs
@@ -1,10 +1,14 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace CashRegister
 {
 	public class Printer
 	{
+		public bool HasPrinted { get; set; }
 		public void Print(string content)
 		{
 			// send message to a real printer
+			HasPrinted = true;
 		}
 	}
 }
